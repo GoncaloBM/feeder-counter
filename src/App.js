185610de885c3fa123bc5dc09c5ca4e86aac7754
Goncalo Mira <goncalomira99@gmail.com>
2020-08-31 13:22:07 +0100

    Date formated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
+import { formatDate } from "./formatDate";
 import "./App.css";
 
 function App() {
   const [value, onChange] = useState(new Date());
+  const [dateFormated, setDateFormated] = useState("");
+
+  const formatDat = () => {
+    setDateFormated(formatDate(value));
+  };
+
+  useEffect(() => {
+    formatDat();
+  }, [value]);
+
   return (
     <div className="App">
       <div classname="calendar">
