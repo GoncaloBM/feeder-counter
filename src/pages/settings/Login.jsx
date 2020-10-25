d99e2b5087aa5df52155658bd29738bb6d963bb1
Goncalo Mira <goncalomira99@gmail.com>
2020-10-25 16:58:25 +0000

    cookies working

--- src/pages/settings/Login.jsx
+++ src/pages/settings/Login.jsx
@@ -1,17 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import "./Login.css";
 import { url } from "../../url";
 import SettingsContext from "../../SettingContext";
+import { LoginTrue } from "./login/LoginTrue";
 
 export const Login = () => {
   const { settings } = useContext(SettingsContext);
   const [stateSettings, setStateSettings] = settings;
 
+  axios.defaults.withCredentials = true;
+
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
 
-  const { login, server ,online} = url;
+  const { login, server, online } = url;
 
   const inputUser = (e) => {
     setUser(e.target.value);
@@ -28,7 +31,7 @@ export const Login = () => {
     };
 
     axios
-      .post(`${online}${login}`, userToLogin)
+      .post(`${server}${login}`, userToLogin)
       .then((res) => {
         console.log(res.data);
         setStateSettings((prevState) => ({
@@ -45,25 +48,53 @@ export const Login = () => {
       });
   };
 
+  const checkLogin = () => {
+    console.log(stateSettings.user.loggedIn);
+    axios
+      .get(`${server}${login}`)
+      .then((res) => {
+        console.log(res);
+        setStateSettings((prevState) => ({
+          ...prevState,
+          user: {
+            ...prevState.user,
+            loggedIn: res.data.loggedIn,
+            username: res.data.username,
+          },
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
   return (
     <div className="login">
-      <form action="">
-        <div className="input">
-          <div className="login-text">User</div>
-          <input type="text" className="login-user" onChange={inputUser} />
-        </div>
-        <div className="input">
-          <div className="login-text">Password</div>
-          <input
-            type="password"
-            className="login-password"
-            onChange={inputPassword}
-          />
-        </div>
-        <div className="button-login" onClick={loginUser}>
-          Login In
-        </div>
-      </form>
+      {!stateSettings.user.loggedIn ? (
+        <form action="">
+          <div className="input">
+            <div className="login-text">User</div>
+            <input type="text" className="login-user" onChange={inputUser} />
+          </div>
+          <div className="input">
+            <div className="login-text">Password</div>
+            <input
+              type="password"
+              className="login-password"
+              onChange={inputPassword}
+            />
+          </div>
+          <div className="button-login" onClick={loginUser}>
+            Login In
+          </div>
+        </form>
+      ) : (
+        <LoginTrue user={stateSettings.user.username} />
+      )}
     </div>
   );
 };
---
+++ src/pages/settings/login/LoginTrue.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LoginTrue = ({ user }) => {
+  return <div className="user">{user}</div>;
+};
