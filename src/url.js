5e35fc173159714bf482dc225eed306ccce0521d
Goncalo Mira <goncalomira99@gmail.com>
2020-11-01 14:08:37 +0000

    urls fixed

--- src/url.js
+++ src/url.js
@@ -1,9 +1,9 @@
 export const url = {
   server: "http://localhost:3001/",
-  online: "https://goncalobmira.online/",
+  online: "https://goncalobmira.online/api",
   getAndPostFeeder: {
     server: "http://localhost:3001/babyfeeder/feeders",
-    online: "https://goncalobmira.online/babyfeeder/feeders",
+    online: "https://goncalobmira.online/api/babyfeeder/feeders",
   },
   register: "babyfeeder/register",
   login: "babyfeeder/login",
